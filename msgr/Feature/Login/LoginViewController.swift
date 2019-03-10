e5ce46931f2177844de9e78aab7e4c6199209a7f
Hideaki Tanabe <tanablog@gmail.com>
2019-03-10 12:53:36 +0900

    wip login feature

--- msgr/Feature/Login/LoginViewController.swift
+++ msgr/Feature/Login/LoginViewController.swift
@@ -3,18 +3,34 @@
 //  msgr
 //
 //  Created by hideaki_tanabe on 2019/03/09.
-//  Copyright © 2019年 hideaki_tanabe. All rights reserved.
+//  Copyright © 2019年 Hideaki Tanabe. All rights reserved.
 //
 
+import Firebase
+import FirebaseAuth
+import FirebaseMessaging
+import RxSwift
+import RxCocoa
+import SwiftyUserDefaults
 import UIKit
 
 class LoginViewController: UIViewController {
 
+    let disposeBag: DisposeBag = DisposeBag()
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        FirebaseApp.configure()
+        bind()
     }
 
+    func bind() {
+    }
 
+    func login(email: String, password: String) {
+    }
 }
-
